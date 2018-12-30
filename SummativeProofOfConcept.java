a626cf5450ef640a3673e601f63d099515f6c85b
Glaze Glop <glazeglopmike@gmail.com>
2018-12-29 21:48:29 -0500

    Initial commit, setup

---
+++ SummativeProofOfConcept.java
@@ -0,0 +1,13 @@
+// The "SummativeProofOfConcept" class.
+import java.awt.*;
+import hsa.Console;
+
+public class SummativeProofOfConcept {
+    private static Console c;
+    
+    public static void main (String[] args) {
+	c = new Console("Dating Simulator");
+	
+	// Place your program here.  'c' is the output console
+    }
+}
